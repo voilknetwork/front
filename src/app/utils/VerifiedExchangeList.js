b551af192bb7c2ef4d09977cb9a577016f879bca
baaluo <berycoin@gmail.com>
2020-09-14 13:29:25 +0500

    removed exchanges referrences

--- src/app/utils/VerifiedExchangeList.js
+++ src/app/utils/VerifiedExchangeList.js
@@ -1,14 +1,7 @@
 const list = `
-bittrex
-blocktrades
-changelly
-deepcrypto8
-gopax-deposit
-hitbtc-exchange
-poloniex
-upbit-exchange
+
 `
-    .trim()
-    .split('\n');
+  .trim()
+  .split("\n");
 
 export default list;
